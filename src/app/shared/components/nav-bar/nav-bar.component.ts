5e8abc4256c3add61213b215c11e958d6df9dd2f
lenoxalmonacid <lalmonacid@belatrixsf.com>
2019-06-13 17:20:24 -0500

    update styles

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-  src = '../../../assets/logo.gif';
+  src = 'assets/logo.gif';
 
   constructor() {}
 
