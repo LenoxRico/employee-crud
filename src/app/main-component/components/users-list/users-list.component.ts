1eeed8d08101b5bad38b8db988e293486cc195f8
Daniel Esteban Puerta <de.puerta@uniandes.edu.co>
2019-06-13 21:45:36 -0500

    Cause of the lack finded

--- src/app/main-component/components/users-list/users-list.component.ts
+++ src/app/main-component/components/users-list/users-list.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee, SeniorityTypes, SeniorityIcons } from '../../interfaces';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { getEmployees } from '../../actions';
-import { EmployeeState } from '../../reducers';
-import { map } from 'rxjs/operators';
 import { MatDialog } from '@angular/material';
 import { EmployeeModalComponent } from '../employee-modal';
 @Component({
@@ -13,18 +11,18 @@ import { EmployeeModalComponent } from '../employee-modal';
   styleUrls: ['./users-list.component.scss']
 })
 export class UsersListComponent implements OnInit {
-  employeeStorage$: Observable<EmployeeState>;
+  employeeStorage$: Observable<Employee[]>;
   seniorityTypes = SeniorityTypes;
   title = 'Employee List';
 
   constructor(private dialog: MatDialog, private store: Store<any>) {
-    this.employeeStorage$ = store.select('employee');
+    this.employeeStorage$ = this.store.select(state=>state.employee.employees);
   }
 
   ngOnInit() {
     this.store.dispatch(getEmployees());
-    this.employeeStorage$.subscribe(state => {
-      state.employees.map(e => {
+    this.employeeStorage$.subscribe(employees => {
+      employees.map(e => {
         if (e.employee_salary < 10000) {
           e.icon = SeniorityIcons.JUNIOR;
         } else if (e.employee_salary >= 10000 && e.employee_salary < 40000) {
--- src/app/main-component/effects/employee.effects.ts
+++ src/app/main-component/effects/employee.effects.ts
@@ -3,7 +3,7 @@ import { Effect, Actions, ofType } from '@ngrx/effects';
 import { EmployeeActions, getEmployees, getEmployeesError, getEmployeesSuccess } from '../actions/employee.actions';
 import { EmployeeService } from '../services';
 import { Observable, of } from 'rxjs';
-import { switchMap, map, catchError, filter } from 'rxjs/operators';
+import { switchMap, map, catchError, take } from 'rxjs/operators';
 import { CoreService } from '@src/app/shared/services';
 
 @Injectable()
@@ -19,7 +19,7 @@ export class EmployeeEffect {
     }),
     map(employees => {
       this.coreServices.displaySpinner(false);
-      return { type: getEmployeesSuccess.type, payload: employees.filter(e => e.employee_age > 0) };
+      return { type: getEmployeesSuccess.type, payload: employees.filter((e,index) => e.employee_age > 0 && index<400) };
     }),
     catchError(() => {
       this.coreServices.displaySpinner(false);
